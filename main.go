02a3f290810a41439ced0e69c4cd258a52dff758
Joao Lima <joaovictorbernardinodelima@gmail.com>
2024-12-27 12:06:44 -0300

    cria struct Tarefas e a funcao que adiciona uma tarefa

--- main.go
+++ main.go
@@ -2,6 +2,15 @@ package main
 
 import "fmt"
 
+type Tarefas struct {
+	ID int
+	Descrição string
+	Concluida bool
+}
+
+var tarefas []Tarefas
+var nextId int = 1
+
 func main() {
 	fmt.Println("Bem vindo ao TODO LIST CLI")
 	mostrarMenu()
@@ -15,3 +24,16 @@ func mostrarMenu(){
 	fmt.Println("4. Remover Tarefa")
 	fmt.Println("5. Sair")
 }
+
+func adicionarTarefas(descricao string) {
+	tarefa := Tarefas {
+		ID: nextId,
+		Descrição: descricao,
+		Concluida: false,
+	}
+
+	tarefas = append(tarefas, tarefa)
+	nextId++
+	fmt.Println("Tarefa criada.")
+}
+
